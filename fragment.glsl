8d1a31b7654cdd01420dea05fed070d35b365b5d
Evgeny Nikiforov <enikiforov@devexperts.com>
2023-07-09 02:58:51 +0300

    colors

--- fragment.glsl
+++ fragment.glsl
@@ -1,3 +1,6 @@
+
+varying vec2 uv;
+
 void main() {
-    gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+    gl_FragColor = vec4((uv + 1.) / 2., 1.0, 1.0);
 }
--- vertex.glsl
+++ vertex.glsl
@@ -1,5 +1,8 @@
 attribute vec2 pos;
 
+varying vec2 uv;
+
 void main() {
+    uv = pos;
     gl_Position = vec4(pos, 1.0, 1.0);
 }
