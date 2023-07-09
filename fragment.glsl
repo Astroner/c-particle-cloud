5c112d3eff7aa7b5435084f83bc16eb47ab2905c
Evgeny Nikiforov <enikiforov@devexperts.com>
2023-07-09 03:13:34 +0300

    optimization

--- fragment.glsl
+++ fragment.glsl
@@ -4,7 +4,9 @@ varying vec2 uv;
 void main() {
     vec2 normalized = (uv * vec2(1., -1.) + 1.) / 2.;
 
-    float ratio = (.5 - (normalized.x + normalized.y)) * 1.001 / 4.;
+    float light = (.5 - (normalized.x + normalized.y)) / 4.;
 
-    gl_FragColor = vec4(.4 + ratio, .4 + ratio, .4 + ratio, 1.0);
+    float color = .4 + light;
+
+    gl_FragColor = vec4(color, color, color, 1.0);
 }
